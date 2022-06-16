9a9b51077bfc12c2ff242ea299f2b6d69413fe53
Igorrteixeira <102987283+Igorrteixeira@users.noreply.github.com>
2022-06-16 18:12:05 -0300

    criei-card-produtos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import  CardProdutos  from './components/cardProdutos';
+import camiseta1 from "./img/produtos/camiseta1.jpg"
+import camiseta2 from "./img/produtos/camiseta2.jpg"
+import camiseta3 from "./img/produtos/camiseta3.jpg"
+import camiseta4 from "./img/produtos/camiseta4.jpg"
+import camiseta5 from "./img/produtos/camiseta5.jpg"
+import camiseta6 from "./img/produtos/camiseta6.jpg"
 
-function App() {
+class App  extends React.Component{
+  state = {
+     produtos:[
+    {
+      id: Date.now(),
+      nome: 'Camiseta',
+      preco: 10,
+      foto: camiseta1,
+    },
+    {
+      id: Date.now(),
+      nome: 'Camiseta',
+      preco: 10,
+      foto: camiseta2,
+    },
+    {
+      id: Date.now(),
+      nome: 'Camiseta',
+      preco: 10,
+      foto: camiseta3,
+    },
+    {
+      id: Date.now(),
+      nome: 'Camiseta',
+      preco: 10,
+      foto: camiseta4,
+    },
+    {
+      id: Date.now(),
+      nome: 'Camiseta',
+      preco: 10,
+      foto: camiseta5,
+    },
+    {
+      id: Date.now(),
+      nome: 'Camiseta',
+      preco: 10,
+      foto: camiseta6,
+    },
+  ]
+}
+  render(){
+    const usuarioComponentes = this.state.produtos.map((produto) => 
+    {console.log(produto)
+       
+      return<CardProdutos
+          id={produto.id}
+          nome={produto.nome}
+          preco={produto.preco}
+          foto={produto.foto} 
+        /> 
+    })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='containerProdutos'>{usuarioComponentes}</div>
     </div>
   );
+  }
 }
-
-export default App;
+export default App
